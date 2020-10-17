2f1e7ee68f9a61c29363095e519805483f87c62c
Кириченко Евгения <ekirichenko@rapidsoft.ru>
2020-10-17 17:48:34 +0300

    feat: execute the task solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    if (!matrix || matrix == []) return []
+      let m = matrix.length,  matrixSort = []; 
+      for (let i = 0; i < m; i++)
+       { 
+      matrixSort[ i ] = [];
+      let n = matrix[i].length;
+      if(i%2 === 0){
+        for (let j = 0; j < n; j++) {
+          matrixSort[ i ][ j ] = matrix[ i ][ j ];
+        }
+      } else {
+        for (let j = 0; j < n; j++) {
+          matrixSort[ i ][ j ] = matrix[ i ][ n-1 - j ];
+        }
+      }  
+    }
+    let res = matrixSort.reduce((result,element) => result.concat(element), []);
+    return res;
+  };
+
